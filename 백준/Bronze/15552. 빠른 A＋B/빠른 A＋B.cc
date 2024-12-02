30e8516aceda6e52a287b40057acc72ad7fe92a4
OhBangIll <95potter95@gmail.com>
2024-12-02 20:57:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,a,b;
+int main(void){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i =0; i < n; i++){
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
