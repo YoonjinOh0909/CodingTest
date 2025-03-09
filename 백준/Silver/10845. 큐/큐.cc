35f44c49a946a2f9509c6f2195250c694a202280
OhYoonjin <95potter95@gmail.com>
2025-03-09 22:17:22 +0900

    [Silver IV] Title: 큐, Time: 208 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+queue<int> q;
+int main(void){
+    cin >> n;
+    while(n--){
+        string s;
+        int val;
+        cin >> s;
+        if(s == "push"){
+            cin >> val;
+            q.push(val);
+        }
+        else if(s == "front"){
+            if(!q.empty()){
+                cout << q.front() << "\n";
+            }else{
+                cout << "-1" << "\n";
+            }
+            
+        }
+        else if( s== "back"){
+            if(!q.empty()){
+                cout << q.back() << "\n";
+            }else{
+                cout << "-1" << "\n";
+            }
+            
+        }
+        else if( s == "pop"){
+            if(!q.empty()){
+                
+                cout << q.front() << "\n";
+                q.pop();
+            }else{
+                cout << "-1" << "\n";
+            }
+        }else if( s== "empty"){
+            if(!q.empty()){
+                cout << "0" << "\n";
+            }else{
+                cout << "1" << "\n";
+            }
+        }else if(s=="size"){
+            cout << q.size() << "\n";
+        }
+    }
+    return 0;
+}
