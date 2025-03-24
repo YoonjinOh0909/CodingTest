d335aa1c62e6b199d87ad134edb4f1d3610e68d3
OhYoonjin <95potter95@gmail.com>
2025-03-24 11:25:00 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 10000 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,68 +1,93 @@
-// Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
-// http://boj.kr/aed4ec552d844acd8853111179d5775d
 #include <bits/stdc++.h>
+
 using namespace std;
+
+int n, m;
+int board[1002][1002] = {0,};
+int jdis[1002][1002] = {0,};
+
+
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
 #define X first
 #define Y second
-string board[1002];
-int dist1[1002][1002]; // 불의 전파 시간
-int dist2[1002][1002]; // 지훈이의 이동 시간
-int n, m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  for(int i = 0; i < n; i++){
-    fill(dist1[i], dist1[i]+m, -1);
-    fill(dist2[i], dist2[i]+m, -1);    
-  }
-  for(int i = 0; i < n; i++)
-    cin >> board[i];  
-  queue<pair<int,int> > Q1;
-  queue<pair<int,int> > Q2;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(board[i][j] == 'F'){
-        Q1.push({i,j});
-        dist1[i][j] = 0;        
-      }
-      if(board[i][j] == 'J'){
-        Q2.push({i,j});
-        dist2[i][j] = 0;
-      }
-    }
-  }
-  // 불에 대한 BFS
-  while(!Q1.empty()){
-    auto cur = Q1.front(); Q1.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue; 
-      dist1[nx][ny] = dist1[cur.X][cur.Y]+1;
-      Q1.push({nx,ny});
-    }
-  }
 
-  // 지훈이에 대한 BFS
-  while(!Q2.empty()){
-    auto cur = Q2.front(); Q2.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
-        cout << dist2[cur.X][cur.Y]+1; 
-        return 0;
-      }
-      if(dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-      if(dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.X][cur.Y]+1) continue; // 불의 전파 시간을 조건에 추가
-      dist2[nx][ny] = dist2[cur.X][cur.Y]+1;
-      Q2.push({nx,ny});
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    queue<pair<int,int>> fireq;
+    queue<pair<int,int>> jq;
+    
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0; j < m; j++){
+            char c;
+            cin >> c;
+
+            if(c == 'F'){
+                board[i][j] = 1;
+                fireq.push({i,j});
+            }
+            if(c == 'J'){
+                jdis[i][j] = 1;
+                jq.push({i,j});
+            }
+            if(c == '#'){
+                board[i][j] = -1;
+                jdis[i][j] = -1;
+            }
+        }
     }
-  }
-  cout << "IMPOSSIBLE"; // 여기에 도달했다는 것은 탈출에 실패했음을 의미.
+
+    while(!fireq.empty()){
+        auto cur = fireq.front(); fireq.pop();
+
+        for(int i =0; i<4; i++){
+            int nx = cur.X + dx[i];
+            int ny = cur.Y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >=n || ny >= m){
+                continue;
+            }
+            if(board[nx][ny] > 0|| board[nx][ny] == -1){ //지나갔거나 벽이거나
+                continue;
+            }
+
+            board[nx][ny] = board[cur.X][cur.Y] + 1;
+            fireq.push({nx,ny});
+            
+        }
+    }
+
+    while(!jq.empty()){
+        auto cur = jq.front(); jq.pop();
+
+        for(int i =0; i<4; i++){
+            int nx = cur.X + dx[i];
+            int ny = cur.Y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >=n || ny >= m){ // 끝에 도달했을 때
+                cout << jdis[cur.X][cur.Y];
+                return 0;
+            }
+            if(jdis[nx][ny] > 0 || board[nx][ny] == -1) continue; // 들렸거나 벽이 있을 때
+            if(board[nx][ny] != 0 && board[nx][ny] <= jdis[cur.X][cur.Y]+1){
+                continue;
+            }
+
+            jdis[nx][ny] = jdis[cur.X][cur.Y] + 1;
+            jq.push({nx,ny});
+            
+        }
+    }
+
+    cout << "IMPOSSIBLE";
+    
+
+    return 0;
 }
