517c88d22c70bef1bc9c47ab0f86c71eeb934319
OhYoonjin <95potter95@gmail.com>
2025-03-27 10:57:58 +0900

    [Silver II] Title: 종이의 개수, Time: 332 ms, Memory: 20720 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,58 +1,54 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-int width;
-int board[2200][2200] = {0,};
+
+int n;
+int board[2188][2188] = {0};
 int cnt[3] = {0,};
 
-bool check(int nxS, int nxE, int nyS, int nyE){
-    // cout << "nxS " << nxS << " nyS " << nyS << "\n";
-    int val = board[nxS][nyS];
+bool checkPaper(int x, int y, int nn){
+    int initVal = board[x][y];
 
-    for(int i = nxS; i < nxE; i++){
-        for(int j = nyS; j < nyE; j++){
-            if(val != board[i][j]) return false;
-        }   
+    for(int i = x; i < x+ nn; i++){
+        for(int j = y; j < y +nn; j++){
+            if(initVal != board[i][j]){
+                return false;
+            }
+        }
     }
-
-    cnt[val+1] = cnt[val+1] +1;
+    
+    cnt[initVal+1]++;
     return true;
 }
 
-int func1(int nxS, int nxE, int nyS, int nyE){
-    
-    if(check(nxS, nxE, nyS, nyE)){
-        
-        return 0 ;
+void rePaper(int x, int y, int nn){
+    if(checkPaper(x,y,nn)){
+        return;
     }
 
-    int wid = nyE - nyS;
-
-    for(int k =0; k < 3; k++){
+    int nextN = nn/3;
+    for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            func1(nxS + wid*k/3, nxS + wid*(k+1)/3, nyS + wid*j/3, nyS + wid*(j+1)/3);
-        }
+            rePaper(x + i*(nextN), y + j*(nextN), nextN);
+        }    
     }
-
-    return 0;
 }
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> width;
-
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j < width; j++){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             cin >> board[i][j];
-        }
+        }    
     }
 
-    func1(0, width, 0, width);
-
-    for(auto c : cnt){
+    rePaper(0,0,n);
+    
+    for(int c : cnt){
         cout << c << "\n";
     }
+
     return 0;
 }
